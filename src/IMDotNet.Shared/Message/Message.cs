904f8557c22d01cb5ba210a2f318b5e049fb02b0
QRWells <mr.qrwang@gmail.com>
2022-03-05 23:51:39 +0900

    Refactored Message's struct

--- src/IMDotNet.Shared/Message/Message.cs
+++ src/IMDotNet.Shared/Message/Message.cs
@@ -4,47 +4,83 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // 
 // This file is part of Project IMDotNet.Shared.
-// File Name   : Packet.cs
+// File Name   : IMessage.cs
 // Author      : Qirui Wang
-// Created at  : 2022/03/03 20:16
+// Created at  : 2022/03/05 14:46
 // Description :
 
 #endregion
 
-using System;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-
 namespace IMDotNet.Shared.Message;
 
-public struct Message
+public abstract class Message
 {
-    public readonly MessageHeader Header;
-    public readonly string? Body;
+    protected MessageHeader _header;
+    protected byte[] _rawBody;
 
-    public Message(MessageHeader header, string? body)
-    {
-        Header = header;
-        Body = body;
-    }
+    public MessageHeader Header => _header;
 
-    public Message(ref ReadOnlySpan<byte> byteBuffer)
-    {
-        if (byteBuffer.Length < 16)
-            throw new ArgumentException
-                ($"{nameof(byteBuffer)} is incomplete.");
-        var header = MemoryMarshal.Cast<byte, ushort>(byteBuffer[..16]);
-        Header = new MessageHeader(header);
-        Body = Encoding.UTF8.GetString(byteBuffer[16..]);
-    }
-
-    public byte[] WriteBuffer()
+    public virtual byte[] WriteBuffer()
     {
         var header = Header.WriteBytes();
-        if (Body == null)
-            return header;
-        var body = Encoding.UTF8.GetBytes(Body);
-        return header.ToArray().Concat(body).ToArray();
+        return header.ToArray().Concat(_rawBody).ToArray();
     }
+
+    protected void ExtractRawMessage(ReadOnlySpan<byte> buffer)
+    {
+        if (!MessageHeader.ReadFromBytes(buffer, out _header)) return;
+        if (buffer.Length < MessageHeader.Size + _header.PayloadLength) return;
+        _rawBody = new byte[_header.PayloadLength];
+        buffer[20..].CopyTo(_rawBody);
+    }
+
+    #region Set Basic Information
+
+    public Message WithSenderId(uint senderId)
+    {
+        _header.SenderId = senderId;
+        return this;
+    }
+
+    public Message WithReceiverId(uint receiverId)
+    {
+        _header.ReceiverId = receiverId;
+        return this;
+    }
+
+    public Message WithPayloadLength(uint payloadLength)
+    {
+        _header.PayloadLength = payloadLength;
+        return this;
+    }
+
+    public Message WithSequenceId(ushort sequenceId)
+    {
+        _header.SequenceId = sequenceId;
+        return this;
+    }
+
+    #endregion
+
+    #region Flag modification
+
+    public Message ClearFlag()
+    {
+        _header.Flag &= 0;
+        return this;
+    }
+
+    public Message IsBroadCast()
+    {
+        _header.Flag |= MessageFlag.BroadCast;
+        return this;
+    }
+
+    public Message IsToGroup()
+    {
+        _header.Flag |= MessageFlag.Group;
+        return this;
+    }
+
+    #endregion
 }
